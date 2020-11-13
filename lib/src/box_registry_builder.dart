a3bf68c856e1338160a94ca560f6d38813da2f6c
Stijn Van Bael <stijn.vanbael@sweetmustard.be>
2020-11-13 19:49:25 +0100

    Support for enums and value objects

--- lib/src/box_registry_builder.dart
+++ lib/src/box_registry_builder.dart
@@ -11,7 +11,8 @@ class BoxRegistryBuilder extends GeneratorForAnnotation<Entity> {
   final List<String> types = [];
 
   @override
-  FutureOr<String> generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) {
+  FutureOr<String> generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
     if (element.kind != ElementKind.CLASS) {
       throw 'ERROR: @Entity can only be used on a class, found on $element';
     }
@@ -45,8 +46,10 @@ class BoxRegistryBuilder extends GeneratorForAnnotation<Entity> {
     ''';
   }
 
-  String _buildFieldTypes(EntityInspector inspector) =>
-      inspector.fields.where(_fieldFilter).map((field) => "'${field.name}': ${field.type.element.name}").join(', ');
+  String _buildFieldTypes(EntityInspector inspector) => inspector.fields
+      .where(_fieldFilter)
+      .map((field) => "'${field.name}': ${field.type.element.name}")
+      .join(', ');
 
   String _buildFieldAccessors(EntityInspector inspector) =>
       '{' +
@@ -64,7 +67,8 @@ class BoxRegistryBuilder extends GeneratorForAnnotation<Entity> {
           ? '(entity) => entity.${inspector.keys.first.name}'
           : '(entity) => Composite({${inspector.keys.map((field) => "'${field.name}': entity.${field.name}").join(', ')}})';
 
-  String _generateDeserializer(EntityInspector inspector, String typeName) => 'map != null '
+  String _generateDeserializer(EntityInspector inspector, String typeName) =>
+      'map != null '
       '? $typeName(${inspector.fields.map((field) => '${field.name}: ${_deserializeType(field.type, "map['${field.name}']")}').join(', ')})'
       ': null';
 
@@ -76,13 +80,19 @@ class BoxRegistryBuilder extends GeneratorForAnnotation<Entity> {
     if (_isPrimitive(type)) {
       return input;
     } else if (type.isDartCoreList) {
-      return _serializeList((type as ParameterizedType).typeArguments.first, input);
+      return _serializeList(
+          (type as ParameterizedType).typeArguments.first, input);
     } else if (type.isDartCoreSet) {
-      return _serializeSet((type as ParameterizedType).typeArguments.first, input);
+      return _serializeSet(
+          (type as ParameterizedType).typeArguments.first, input);
     } else if (_isType(type, 'dart.core', 'DateTime')) {
       return '$input?.toIso8601String()';
-    } else {
+    } else if (_isEnum(type.element)) {
+      return 'serializeEnum($input)';
+    } else if (_isEntity(type.element)) {
       return 'serializeEntity($input)';
+    } else {
+      return '$input?.toJson()';
     }
   }
 
@@ -90,13 +100,19 @@ class BoxRegistryBuilder extends GeneratorForAnnotation<Entity> {
     if (_isPrimitive(type)) {
       return input;
     } else if (type.isDartCoreList) {
-      return _deserializeList((type as ParameterizedType).typeArguments.first, input);
+      return _deserializeList(
+          (type as ParameterizedType).typeArguments.first, input);
     } else if (type.isDartCoreSet) {
-      return _deserializeSet((type as ParameterizedType).typeArguments.first, input);
+      return _deserializeSet(
+          (type as ParameterizedType).typeArguments.first, input);
     } else if (_isType(type, 'dart.core', 'DateTime')) {
       return 'deserializeDateTime($input)';
-    } else {
+    } else if (_isEnum(type.element)) {
+      return 'deserializeEnum($input, ${type.element.name}.values)';
+    } else if (_isEntity(type.element)) {
       return 'deserializeEntity<${type.element.name}>($input)';
+    } else {
+      return '$input != null ? ${type.element.name}.fromJson($input) : null';
     }
   }
 
@@ -110,6 +126,9 @@ class BoxRegistryBuilder extends GeneratorForAnnotation<Entity> {
 
   bool _isType(DartType type, String library, String typeName) =>
       type.element.library.name == library && type.element.name == typeName;
+
+  bool _isEntity(Element element) => element.metadata.any((metadata) =>
+      _isType(metadata.computeConstantValue().type, 'box.core', 'Entity'));
 
   String _deserializeList(DartType type, String input) => '$input != null '
       '? List<${type.element.name}>.from($input.map((element) => ${_deserializeType(type, 'element')})) '
@@ -126,6 +145,8 @@ class BoxRegistryBuilder extends GeneratorForAnnotation<Entity> {
   String _serializeSet(DartType type, String input) => '$input != null '
       '? $input.map((element) => ${_serializeType(type, 'element')}).toSet() '
       ': null';
+
+  bool _isEnum(Element element) => element is ClassElement && element.isEnum;
 }
 
 class EntityInspector extends SimpleElementVisitor<void> {
@@ -146,7 +167,8 @@ class EntityInspector extends SimpleElementVisitor<void> {
 
   bool _isKey(ElementAnnotation element) {
     var value = element.computeConstantValue();
-    return value.type.element.library.name == 'box.core' && value.type.element.name == 'Key';
+    return value.type.element.library.name == 'box.core' &&
+        value.type.element.name == 'Key';
   }
 
   @override
